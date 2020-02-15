52287b1680fd10d27850d42307b150d8c64f2d5f
VillePihlava <ville.pihlava@aalto.fi>
2020-02-15 17:41:48 +0200

    Fix untracked files

--- JavaScriptLunarLander/settings.py
+++ JavaScriptLunarLander/settings.py
@@ -17,11 +17,9 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@7w(a68@*)sb7!m%^g9aq)j4okpgwlh9mbk=0)ypt88)id^it!'
+# Use a separate file for the secret key
+with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_generate_secret_key',
     'lunarlander',
 ]
 
